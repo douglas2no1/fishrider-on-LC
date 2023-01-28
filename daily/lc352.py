46d8bbe1ae6c7ae8ff6edea93a66bb625f8fb702
yujie zhang <yujiezhang@yujiedeMacBook-Pro.local>
2023-01-27 23:06:42 -0800

    push lc352

---
+++ daily/lc352.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class SummaryRanges:
+
+    def __init__(self):
+        pass
+
+    def addNum(self, value: int) -> None:
+        pass
+
+    def getIntervals(self) -> List[List[int]]:
+        pass
+# Your SummaryRanges object will be instantiated and called as such:
+# obj = SummaryRanges()
+# obj.addNum(value)
+# param_2 = obj.getIntervals()
